addf23a4123acbc024571ad85f7b8bd905d7b8b2
jitendra19 <jiten.cs2010@gmail.com>
2021-11-20 00:06:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# debounce-methods-implementation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/debounce-methods-implementation)
---
+++ debounce.js
@@ -0,0 +1,19 @@
+// console.log('hello!')
+
+const getData = (a) => {
+  const ele = document.getElementById('typeDebounce');
+  console.log('fetch data...', ele.value);
+};
+const debounce = (fn, delay) => {
+  let timer;
+  return () => {
+    let context = this;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      // fn.apply(context);
+      fn();
+    }, delay);
+  };
+};
+const withDebounce = debounce(getData, 400);
+// const withDebounce = getData;
