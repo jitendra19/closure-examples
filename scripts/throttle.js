7c72920c9105cd7a6629f322a6200308edc9b8c5
jitendra19 <jiten.cs2010@gmail.com>
2021-11-20 00:17:01 +0530

    throttle function

---
+++ scripts/throttle.js
@@ -0,0 +1,18 @@
+const fireMe = () => {
+  const inputValue = document.getElementById('typeDebounce').value;
+  console.log('event is fired with input value', inputValue);
+};
+
+const throttle = (fn, delay) => {
+  let timer;
+  return () => {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn();
+        timer = '';
+      }, delay);
+    }
+  };
+};
+
+const withThrottle = throttle(fireMe, 1000);
