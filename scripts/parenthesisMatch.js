e9adabfff1d29f8916bfd91aa847fc3e48dce67b
jitendra singhal <jiten.cs2010@gmail.com>
2021-12-26 22:36:24 +0530

    parenthesis match implementation

--- scripts/parenthesisMatch.js
+++ scripts/parenthesisMatch.js
@@ -1,5 +1,23 @@
-export default function parenthesisMatch(s) {
+function parenthesisMatch(s) {
+  if (s.length % 2 !== 0) return false;
+  const stack = [];
+  for (let i of s.split("")) {
+    if (["(", "["].indexOf(i) > -1) {
+      stack.push(i);
+    } else {
+      if (
+        (i === "]" && stack[stack.length - 1] === "[") ||
+        (i === ")" && stack[stack.length - 1] === "(")
+      ) {
+        stack.pop();
+      } else {
+        stack.push(i);
+      }
+    }
+  }
+  if (stack.length === 0) return true;
   return false;
 }
 
-parenthesisMatch("[[[[[[]]]]]]]");
+console.log("parenthesis check --- ", parenthesisMatch("[[[[[[]]]]]]]"));
+console.log("parenthesis check --- ", parenthesisMatch("[[[[[[[]]]]]]]"));
